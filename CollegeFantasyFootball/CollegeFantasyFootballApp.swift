249214f460976e3dbb196e1e93b2427d611c060d
Colten Glover <coltenglover@yahoo.com>
2025-04-07 19:39:03 -0400

    Initial Commit

---
+++ CollegeFantasyFootball/CollegeFantasyFootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeFantasyFootballApp.swift
+//  CollegeFantasyFootball
+//
+//  Created by Colten Glover on 4/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CollegeFantasyFootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CollegeFantasyFootballTests/CollegeFantasyFootballTests.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeFantasyFootballTests.swift
+//  CollegeFantasyFootballTests
+//
+//  Created by Colten Glover on 4/7/25.
+//
+
+import Testing
+@testable import CollegeFantasyFootball
+
+struct CollegeFantasyFootballTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
