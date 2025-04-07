6a81e825f9f65f0f7ab9c6c831c1cbf089608a70
Colten Glover <coltenglover@yahoo.com>
2025-04-07 19:53:32 -0400

    Added Supabase connectivity

--- CollegeFantasyFootball/SupabaseManager.swift
+++ CollegeFantasyFootball/SupabaseManager.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import Supabase
+
+/// Singleton class to interface with Supabase
+class SupabaseManager {
+    /// Singleton instance
+    public static let shared = SupabaseManager()
+    /// Connection to Supabase
+    public let client: SupabaseClient
+    
+    /// Constructor to initialize the client
+    private init() {
+        let supabaseUrl = URL(string: Secrets.SUPABASE_URL)!
+        let supabaseKey = Secrets.SUPABASE_KEY
+        
+        client = SupabaseClient(
+            supabaseURL: supabaseUrl,
+            supabaseKey: supabaseKey)
+    }
+}
+
+enum SessionError: Error {
+    case noUsername(String)
+    case noEmail(String)
+}
