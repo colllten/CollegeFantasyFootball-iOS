8cf24d772d48605fab7bb4c9f607ddeb94076294
Colten Glover <coltenglover@yahoo.com>
2025-08-20 14:16:16 -0400

    Added clear text button

---
+++ CollegeFantasyFootball/Views/Roster/RosterView.swift
@@ -0,0 +1,18 @@
+//
+//  RosterView.swift
+//  CollegeFantasyFootball
+//
+//  Created by Colten Glover on 8/20/25.
+//
+
+import SwiftUI
+
+struct RosterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RosterView()
+}
---
+++ CollegeFantasyFootball/Views/Roster/RosterViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  RosterViewModel.swift
+//  CollegeFantasyFootball
+//
+//  Created by Colten Glover on 8/20/25.
+//
+
+import Foundation
